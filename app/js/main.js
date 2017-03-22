7595558575f8d1325e76381f8ab612c2a8a2429f
lymsun <lymsunsheep@sina.cn>
2017-03-22 21:03:10 +0800

    echarts Demo 20170322 at foshan

--- app/js/main.js
+++ app/js/main.js
@@ -6,3 +6,55 @@
  * Function:
  *
  */
+
+(function ($) {
+    var demo1Chart = echarts.init(document.getElementById('demo1Content'));
+
+    demo1Chart.showLoading();
+    $.get('../json/demo1.json').done(function (data) {
+        var opinion = {
+            title: {
+                text: 'demo1'
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                data: ['案件总数', '新增案件数', '办结案件数']
+            },
+            xAxis: {
+                type: 'category',
+                data: data.xCategories
+            },
+            yAxis: [
+                {
+                    type: 'value',
+                    name: '案件数',
+                    
+                    axisLabel: {
+                        formatter: '{value} 件'
+                    }
+                }
+            ],
+            series: [
+                {
+                    name: '案件总数',
+                    type: 'bar',
+                    data: data.aData
+                },
+                {
+                    name: '新增案件数',
+                    type: 'bar',
+                    data: data.bData
+                },
+                {
+                    name: '办结案件数',
+                    type: 'line',
+                    data: data.cData
+                }
+            ]
+        };
+        demo1Chart.hideLoading();
+        demo1Chart.setOption(opinion);
+    });
+})(jQuery);
