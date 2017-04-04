f113cca0c3d2eebf5b342d621249e528c883e695
lymsun <lymsunsheep@sina.cn>
2017-04-04 13:35:00 +0800

    echarts Demo 20170404 at beijing build demo2

--- app/js/demo3.js
+++ app/js/demo3.js
@@ -2,7 +2,25 @@
  *
  * File: demo3.js
  * Author: Lym
- * Created Date: 2017/3/23.
+ * Created Date: 2017/3/18.
  * Function:
  *
  */
+
+(function ($) {
+    var demo3Chart = echarts.init(document.getElementById('demo3Content'));
+
+    demo3Chart.showLoading();
+    $.get('../json/ne_50m_admin_0_countries_lakes.json').done(function (geoJson) {
+        var mapChart = echarts.registerMap('geo', geoJson);
+        var opinion = {
+            geo: {
+                map: 'geo',
+                roam: true
+            }
+        };
+
+        demo3Chart.hideLoading();
+        demo3Chart.setOption(opinion);
+    });
+})(jQuery);
