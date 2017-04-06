4e5ddb43e11f512f8d9fc7991850a5406cd0af2c
lymsun <lymsunsheep@sina.cn>
2017-04-06 19:48:33 +0800

    echarts Demo 20170406 at beijing finish demo3

--- app/js/demo3.js
+++ app/js/demo3.js
@@ -8,16 +8,53 @@
  */
 
 (function ($) {
-    var demo3Chart = echarts.init(document.getElementById('demo3Content'));
+    function randomCountryValue() {
+        return Math.round(Math.random()*10000);
+    }
 
+    var demo3Chart = echarts.init(document.getElementById('demo3Content'));
     demo3Chart.showLoading();
+
     $.get('../json/ne_50m_admin_0_countries_lakes.json').done(function (geoJson) {
-        var mapChart = echarts.registerMap('geo', geoJson);
+        var countryNum = geoJson.features.length;
+        var countryArray = [];
+        for(var i = 0; i < countryNum; i++)
+        {
+            countryArray.push({
+                name: geoJson.features[i].properties.name,
+                value: randomCountryValue()
+            });
+        }
+
+        echarts.registerMap('worldMap', geoJson);
+
         var opinion = {
-            geo: {
-                map: 'geo',
-                roam: true
-            }
+            backgroundColor: '#404a59',
+            title: {
+                text: 'World Map',
+                textStyle: {
+                    color: '#fff'
+                },
+                x: 'left'
+            },
+            visualMap: {
+                type: 'continuous',
+                min: 0,
+                max: 10000,
+                realtime: false,
+                calculable: true,
+                color: ['orangered','yellow','lightskyblue'],
+                textStyle: {
+                    color: '#fff'
+                },
+                y: 'center'
+            },
+            series: [{
+                type: 'map',
+                roam: true,
+                map: 'worldMap',
+                data: countryArray
+            }]
         };
 
         demo3Chart.hideLoading();
