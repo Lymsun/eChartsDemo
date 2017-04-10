3be0f9e95799e5975d2d97268c12d165403b4ada
lymsun <lymsunsheep@sina.cn>
2017-04-10 17:40:41 +0800

    echarts Demo 20170410 at beijing use csvtojson to learn demo4

--- app/js/demo4.js
+++ app/js/demo4.js
@@ -9,14 +9,42 @@
 
 (function ($) {
     function splitData(rawData) {
+        var specificData = [];
+        for (var i = 0; i<rawData.length; i++) {
+            if (rawData[i].Ticker == 'ABC') {
+                specificData.push(rawData[i]);
+            }
+        }
+
         var categoryData = [];
         var values = [];
         var volumes = [];
 
-        for (var i = 0; i< rawData.length; i++ ) {
-            
+        for (var j = 0; j< specificData.length; j++ ) {
+            var dateString = specificData[j].Date;
+            var pattern = /(\d{4})(\d{2})(\d{2})/;
+            var newDateString = dateString.replace(pattern, '$1-$2-$3');
+            categoryData.push(newDateString);
+            values.push(specificData[j].Open, specificData[j].High, specificData[j].Low, specificData[j].Close);
+            volumes.push(specificData[j].Volume);
+        }
+
+        return{
+            categoryData: categoryData,
+            values: values,
+            volumes: volumes
         }
     }
 
+    var demo4Chart = echarts.init(document.getElementById('demo4Content'));
+    demo4Chart.showLoading();
 
+    $.get('../json/stock.json').done(function (stockData) {
+        var data = splitData(stockData);
+
+        var opinion = {};
+
+        demo4Chart.hideLoading();
+        demo4Chart.setOption(opinion);
+    });
 })(jQuery);
